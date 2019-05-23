1ebf9a31060291ab8fce824eed2b1e71d80c2af8
Didier Matos <d.matostorres@gmail.com>
2019-05-23 08:41:40 -0500

    Clase 2: Undefined

--- Undefined/app.js
+++ Undefined/app.js
@@ -1,4 +1,7 @@
-//Hola Mundo
-alert(a) //desde la consola
+//Javascript les asigna un valor undefined a las variables que se utilizan antes de ser creadas.
+
+alert(a) //Se muestra en alert para comprobar rapidamente sin abrir la consola
 
 var a = 10;
+
+alert("despues de asignar valor: " + a);
