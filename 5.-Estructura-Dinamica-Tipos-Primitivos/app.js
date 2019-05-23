dd878cca45ef0232c389c8ca4d008d31ee99cacb
Didier Matos <d.matostorres@gmail.com>
2019-05-23 10:16:34 -0500

    Tipos primitivos, estructura dinamica y objetos

---
+++ 5.-Estructura-Dinamica-Tipos-Primitivos/app.js
@@ -0,0 +1,33 @@
+
+//ESTRUCTURA DINAMICA
+//JavaScript es dinamico al asignar valores diferentes a variables distintas
+/*var nombre = "Juan" 
+
+var num = 10;
+var str = "texto"
+var bol = true
+var und = undefined
+var nul = null
+
+console.log(num)
+
+num = str
+
+console.log (num)
+
+bol = 10*/
+
+//TIPOS PRIMITIVOS
+
+var obj = {
+    numero: 10,
+    texto: "Nuevo texto",
+
+    objHijo: {
+        numero2: 20,
+        texto2: "Nuevo texto 2"
+    }
+
+}
+
+console.log(obj);
