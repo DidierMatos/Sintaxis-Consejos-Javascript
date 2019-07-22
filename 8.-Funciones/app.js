34a356dac1a369647af77bf3e43ea4c02f39fd5d
Didier Matos <d.matostorres@gmail.com>
2019-07-22 12:28:00 -0500

    Fundamentos de las funciones de JS

---
+++ 8.-Funciones/app.js
@@ -0,0 +1,35 @@
+
+var a = 30;
+
+function primeraFuncion(){
+
+    var a = 20;
+
+    console.log( a );
+}
+
+//primeraFuncion();
+
+/////////////////////// 
+
+
+//segundaFuncion();
+
+var b = 30;
+
+function segundaFuncion(){
+
+    var b = 30;
+
+    console.log( a );
+}
+
+/////////////////////// 
+
+function terceraFuncion(){
+
+    console.log("tercera funcion invocada");
+
+}
+
+var miFuncion = terceraFuncion; //llamar en la consolan como mifuncion()
