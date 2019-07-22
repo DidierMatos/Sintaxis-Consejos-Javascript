937143af126caa6546b24f7a608d891f42ea3268
Didier Matos <d.matostorres@gmail.com>
2019-07-22 11:34:05 -0500

    Agregando comentarios

--- 6.-Por valor y referencia/app.js
+++ 6.-Por valor y referencia/app.js
@@ -3,13 +3,13 @@
 var a = 10; // por valor asignado
 var b = a
 
-console.log("a: ", a)
-console.log("b: ", b)
+console.log("a: ", a) //10
+console.log("b: ", b)   //10
 
 a = 20
 
-console.log("a: ", a)
-console.log("b: ", b)
+console.log("a: ", a) //20
+console.log("b: ", b) //20
 
 var c = {
     nombre: "Juana"
@@ -17,15 +17,15 @@ var c = {
 
 var d = c; // por referencia -- es decir ambas son afectas simultaneamente -- es como una regla
 
-console.log("c: ", c)
-console.log("d: ", d)
+console.log("c: ", c) //Juana
+console.log("d: ", d)   //Juana
 
 c.nombre = "Maria"
 
-console.log("c: ", c)
-console.log("d: ", d)
+console.log("c: ", c) //Maria
+console.log("d: ", d) //Maria
 
 d.nombre = "Pedro"
 
-console.log("c: ", c)
-console.log("d: ", d)
+console.log("c: ", c) //Pedro
+console.log("d: ", d) //Pedro
