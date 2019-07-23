4eebeb611c19d1ebfd7e165d10e289f741432f83
Didier Matos <d.matostorres@gmail.com>
2019-07-23 12:23:32 -0500

    Dos formas de usar las funciones anonimas

---
+++ 16.-Funciones-anonimas/app.js
@@ -0,0 +1,34 @@
+
+(function(){
+
+    var a = 10;
+
+    function cambiarA(){
+        a=20;
+    }
+
+    cambiarA();
+
+    console.log(a);
+})();
+
+///////////////////////
+
+function ejecutarFuncion(fn){
+    
+    if( fn() === 1){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(
+
+    ejecutarFuncion(function(){
+        console.log("Funcion anonima ejecutada")
+        return 0;
+    })
+
+);
+
