0f4dfdefe8fd9edaa8c7362da4503ebc3cb5afd0
Didier Matos <d.matostorres@gmail.com>
2019-07-24 14:03:57 -0500

    Arreglos con propiedades como push slice splice split unshift etc

---
+++ app.js
@@ -0,0 +1,39 @@
+var arr = [5,4,3,2,1];
+console.log(arr);
+
+console.log(arr[0], arr[4], arr[5]);
+
+arr.reverse();
+console.log(arr);
+
+arr = arr.map(function(elem){
+    elem *= elem;
+    return elem;
+});
+console.log(arr);
+
+arr = arr.map(Math.sqrt);
+console.log(arr);
+
+arr = arr.join("|");
+console.log(arr);
+
+arr = arr.split("|");
+console.log(arr);
+
+arr.push("6");
+console.log(arr);
+
+arr.unshift("0");
+console.log(arr);
+
+console.log(arr.toString());
+
+var elimine = arr.pop();
+console.log(arr, elimine);
+
+arr.splice(1,3,"10", "20", "30");
+console.log(arr);
+
+arr = arr.slice(0,3)
+console.log(arr);
